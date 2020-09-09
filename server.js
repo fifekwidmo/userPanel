ebb308f5e82e458e4a680cb54259ec172357ecde
Filip Cwiek <fifek.inex@gmail.com>
2020-09-09 11:52:03 +0200

    Clear code

--- server.js
+++ server.js
@@ -5,21 +5,16 @@ const hbs = require('express-handlebars');
 const passport = require('passport');
 const session = require('express-session');
 const passportSetup = require('./config/passport');
-
 const app = express();
 
-// set handlebars as view engine
 app.engine('hbs', hbs({ extname: 'hbs', layoutsDir: './layouts', defaultLayout: 'main' }));
 app.set('view engine', '.hbs');
 
-// init session mechanism
 app.use(session({ secret: 'anything' }));
 
-// init passport
 app.use(passport.initialize());
 app.use(passport.session());
 
-// standard middleware
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
